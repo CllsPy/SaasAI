5b207d83e5e1057fcfc08bf3db2bfd16875604e6
CLL <96326019+CllsPy@users.noreply.github.com>
2025-06-02 18:54:25 +0000

    signup conttroler

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -10,3 +10,14 @@ export const getAllUsers = async (req, res, next) => {
     }
 };
 //# sourceMappingURL=user-controllers.js.map
+
+export const userSignup = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error", case: error.message });
+    }
+};
