0c8965de3c5c12845b9ae28862d81af0bc400c3e
CLL <96326019+CllsPy@users.noreply.github.com>
2025-06-02 19:27:04 +0000

    fix npm delete post and signuproute

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -12,14 +12,13 @@ export const getAllUsers = async (req, res, next) => {
     }
 };
 
-
 export const userSignup = async (req, res, next) => {
-    const { name, email, password } = req.body();
-    const hashedPassword = has(password, 10);
-    const user = new User.find({ name, email, password:hashedPassword })
-    await user.save();
+
     try {
-        const users = await User.find();
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
         return res.status(200).json({ message: "OK", id:user._id.toString() });
     }
     catch (error) {
