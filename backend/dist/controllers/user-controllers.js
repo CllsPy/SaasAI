0ff636a075405e783e5e4be54af6fa8204233a83
CLL <96326019+CllsPy@users.noreply.github.com>
2025-06-10 04:13:25 +0000

    -bugFix => userRoutes

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -6,17 +7,21 @@ export const getAllUsers = async (req, res, next) => {
     }
     catch (error) {
         console.log(error);
-        return res.status(200).json({ message: "Error", case: error.message });
+        return res.status(500).json({ message: "Error", case: error.message });
     }
 };
-export const getAllUsers = async (req, res, next) => {
+export const userSignup = async (req, res, next) => {
     try {
-        const users = await User.find();
-        return res.status(200).json({ message: "OK", users });
+        console.log(req.body); // debugg
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 2);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
-        return res.status(200).json({ message: "Error", case: error.message });
+        return res.status(500).json({ message: "Error", case: error.message });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
