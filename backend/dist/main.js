10fc43079928b99e88bac2ec8a156aca166ed8f0
CLL <96326019+CllsPy@users.noreply.github.com>
2025-06-13 00:20:35 +0000

    start new project

--- backend/dist/main.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-const port = 5000;
-// middledwares
-app.use(express.json());
-export default app;
-//# sourceMappingURL=main.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,3 @@
-import app from "./app.js";
-import { connectToDataBase } from "./db/connection.js";
-
-const PORT = process.env.PORT
-// contections and listeners
-
-connectToDataBase().then(() => {
-  app.listen(PORT, () => {
-  console.log(`Open and Connected to DB`);
-})
-}).catch(err=>console.log(err))
-
+console.log(
+  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
+);
