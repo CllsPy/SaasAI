8c5413a2a6af48a7790cd9327dd4f1f740d0a9a5
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-13 01:21:18 +0000

    --update tested expres.json()

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 const PORT = 5000;
-app.get("/Home", (req, res, next) => {
+app.use(express.json());
+app.post("/Home", (req, res, next) => {
+    console.log(req.body.name);
     return res.send("Oi...");
 });
 app.listen(PORT, () => console.log("RODANDO..."));
