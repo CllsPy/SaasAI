a63f4d3977c2332b2627877b9d4e4f9e310d1157
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-16 18:17:55 +0000

    --bugfix error TS18046

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,3 +1,16 @@
-export const getAllUsers = () => {
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        //get all users
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        if (error instanceof Error) {
+            return res.status(200).json({ message: "ERROR", cause: error.message });
+        }
+        return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,7 +1,23 @@
 
 import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
 
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //get all users
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    
+    if (error instanceof Error) {
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 
-export const getAllUsers = () =>  {
-
+    return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
+  }
 };
