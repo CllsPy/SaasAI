d79bd6257bc1463214ae6e9fb451283beebe445a
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-27 14:55:51 +0000

    --update => loginValidator

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         //get all users
@@ -18,11 +18,37 @@ export const userSignup = async (req, res, next) => {
     try {
         //user signup
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
         return res
             .status(201)
+            .json({ message: "OK", name: user.name, email: user.email });
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            return res.status(200).json({ message: "ERROR", cause: error.message });
+        }
+        return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        //user login
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        return res
+            .status(200)
             .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -32,12 +32,44 @@ export const userSignup = async (
   try {
     //user signup
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already registered");
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
     return res
       .status(201)
+      .json({ message: "OK", name: user.name, email: user.email });
+  } catch (error) {
+    if (error instanceof Error) {
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+
+    return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
+    
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user login
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("User not registered");
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect Password");
+    }
+
+    return res
+      .status(200)
       .json({ message: "OK", name: user.name, email: user.email });
   } catch (error) {
     if (error instanceof Error) {
