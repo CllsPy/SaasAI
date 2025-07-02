4aff01246c09ed5634744d11a3f1d50f65033f12
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-07-02 22:28:27 +0000

    --fix user-controller login/signup routes

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async (req, res, next) => {
     try {
         //get all users
@@ -19,17 +20,35 @@ export const userSignup = async (req, res, next) => {
     try {
         //user signup
         const { name, email, password } = req.body;
-        const existingUser = await User.findOne({ email }); // evitar duplicata
+        const existingUser = await User.findOne({ email });
         if (existingUser)
             return res.status(401).send("User already registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+        // create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
         return res
             .status(201)
             .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
+        console.log(error);
         if (error instanceof Error) {
             return res.status(200).json({ message: "ERROR", cause: error.message });
         }
@@ -48,12 +67,29 @@ export const userLogin = async (req, res, next) => {
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect Password");
         }
+        // create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
         const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
         return res
             .status(200)
             .json({ message: "OK", name: user.name, email: user.email });
     }
     catch (error) {
+        console.log(error);
         if (error instanceof Error) {
             return res.status(200).json({ message: "ERROR", cause: error.message });
         }
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -32,22 +33,42 @@ export const userSignup = async (
   try {
     //user signup
     const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email }); // evitar duplicata
+    const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(401).send("User already registered");
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
+    // create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res
       .status(201)
       .json({ message: "OK", name: user.name, email: user.email });
   } catch (error) {
+    console.log(error);
+    
     if (error instanceof Error) {
         return res.status(200).json({ message: "ERROR", cause: error.message });
     }
 
     return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
-    
   }
 };
 
@@ -68,17 +89,36 @@ export const userLogin = async (
       return res.status(403).send("Incorrect Password");
     }
 
+    // create token and store cookie
+
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
     const token = createToken(user._id.toString(), user.email, "7d");
-    
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res
       .status(200)
       .json({ message: "OK", name: user.name, email: user.email });
-  } catch (error) {
+  }catch (error) {
+    console.log(error);
+    
     if (error instanceof Error) {
         return res.status(200).json({ message: "ERROR", cause: error.message });
     }
 
     return res.status(200).json({ message: "ERROR", cause: "Unknown error" });
-    
   }
 };
