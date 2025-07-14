0ccf33ffdf816083e34a2b4bbda9e39edd60f440
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-07-14 14:52:21 +0000

    --update => appbat; toolbar; start header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return ( 
+  <AppBar sx={{}}>
+    <Toolbar></Toolbar>
+  </AppBar>
+  );
+};
 
 export default Header
