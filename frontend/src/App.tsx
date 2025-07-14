a2f5033c8170114678f4a7931b70750f597b5275
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-07-14 16:03:18 +0000

    --update => authuser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,11 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
 import Chat from "./pages/Chat";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  console.log(useAuth()?.isLoggedIn);
+  
   return (
   <main>
     <Header />
